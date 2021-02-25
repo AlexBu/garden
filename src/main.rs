9963eaaeb3f5ea8ed403655e7b080b60a4867e39
Bu, Kun Peng <bkpfx000@gmail.com>
2021-02-25 18:09:37 +0800

    cross build first prog

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![feature(asm)]
+#![feature(global_asm)]
+#![no_main]
+#![no_std]
+
+global_asm!(include_str!("boot.s"));
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    unimplemented!()
+}
